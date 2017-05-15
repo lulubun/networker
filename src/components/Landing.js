1ad6a539a022a1a3950518121607e36fad2a79c8
lulubun <laurapetitt@gmail.com>
2017-05-15 16:32:31 -0400

    updating components to addresponsive design

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,19 +1,44 @@
 import React from 'react';
-import Star from 'material-ui/svg-icons/action/stars'
+import Star from 'material-ui/svg-icons/action/stars';
+import MediaQuery from 'react-responsive';
+
 
 class Landing extends React.Component {
   render() {
     const style={
       'paddingLeft': '20px'
     }
+
     const starStyle={
       width: '50px'
     }
+
+    const styleMin={
+      display: 'block',
+      paddingLeft: '25px',
+      paddingRight: '35px'
+    }
+
+    const starStyleMin={
+      display: 'none'
+    }
+
     return (
-      <div style={style} className="Landing">
-        <p><Star style={starStyle} className="star" color={"#5D576B"} />Keep track of your network</p>
-        <p><Star style={starStyle} className="star" color={"#5D576B"} />Add follow up reminders to your Google Calendar</p>
-        <p><Star style={starStyle} className="star" color={"#5D576B"} />Record all of your interactions with contacts and take important notes</p>
+      <div>
+      <MediaQuery query='(min-device-width: 1224px)'>
+        <div style={style} className="Landing">
+          <p><Star style={starStyle} className="star" color={"#5D576B"} />Keep track of your network</p>
+          <p><Star style={starStyle} className="star" color={"#5D576B"} />Add follow up reminders to your Google Calendar</p>
+          <p><Star style={starStyle} className="star" color={"#5D576B"} />Record all of your interactions with contacts and take important notes</p>
+        </div>
+      </MediaQuery>
+      <MediaQuery query='(max-device-width: 1224px)'>
+        <div style={styleMin} className="Landing">
+          <p><Star style={starStyleMin} className="star" color={"#5D576B"} />Keep track of your network</p>
+          <p><Star style={starStyleMin} className="star" color={"#5D576B"} />Add follow up reminders to your Google Calendar</p>
+          <p><Star style={starStyleMin} className="star" color={"#5D576B"} />Record all of your interactions with contacts and take important notes</p>
+        </div>
+      </MediaQuery>
       </div>
     )
   }
--- src/components/LoginImg.js
+++ src/components/LoginImg.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import MediaQuery from 'react-responsive';
+
 
 class LoginImg extends React.Component {
 
@@ -11,13 +13,31 @@ class LoginImg extends React.Component {
       letterSpacing: '40px',
       color: 'white',
       webkitTextStroke: '1px #5D576B'
-    }
+    };
+
+    const smTitleStyle = {
+      textAlign: 'center',
+      paddingBottom: '30px',
+      fontSize: '50px',
+      fontStyle: 'oblique',
+      letterSpacing: '5px',
+      color: 'white',
+      webkitTextStroke: '1px #5D576B',
+      paddingRight: '20px',
+    };
 
     return (
       <div className="loginImg">
+        <MediaQuery query='(min-device-width: 1224px)'>
         <div>
           <h1 style={titleStyle}>Networker</h1>
         </div>
+      </MediaQuery>
+      <MediaQuery query='(max-device-width: 1224px)'>
+      <div>
+        <h1 style={smTitleStyle}>Networker</h1>
+      </div>
+    </MediaQuery>
       </div>
     );
   }
